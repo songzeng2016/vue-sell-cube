083fa3f7c2558e11f4ab86acfea5874641e3d87b
songzeng <songzeng62@gmail.com>
2018-11-18 22:33:39 +0800

    init project

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,11 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'object-curly-spacing': 0,
+    'comma-dangle': 0,
+    'space-before-function-paren': 0,
+    'indent': 0
   },
   parserOptions: {
     parser: 'babel-eslint'
